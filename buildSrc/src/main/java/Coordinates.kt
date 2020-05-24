97f6d3c0751c7e24846951aede8719cc2ce0a913
Sertunc SELEN <sertuncselen@yahoo.com>
2020-05-24 18:39:11 +0300

    Update Coordinates.kt

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "com.sertuncselen.appname"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
